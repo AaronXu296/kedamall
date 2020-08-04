8cfb16110aa018e93fced18d0bbaa1e225813625
AaronXu296 <xzy2967@163.com>
2020-08-04 14:27:11 +0800

    spring gateway

---
+++ kedamall-gateway/src/test/java/com/example/kedamall/gateway/KedamallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kedamall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedamallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
