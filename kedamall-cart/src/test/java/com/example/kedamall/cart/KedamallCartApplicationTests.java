70313769496d8768a7ae0bc89a02c50a7bf5e78d
AaronXu296 <xzy2967@163.com>
2020-12-10 20:46:24 +0800

    中级篇之购物车服务（ThreadLocal同一线程共享数据、合并购物车等）

---
+++ kedamall-cart/src/test/java/com/example/kedamall/cart/KedamallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kedamall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedamallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
